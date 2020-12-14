9ddc7a2257add0517e48f05de603f878836a5021
Artemiy Matchin <art16ws@gmail.com>
2020-12-14 21:11:33 +0300

    Basic VR UI

--- Assets/Scenes/Kek/movement.cs
+++ Assets/Scenes/Kek/movement.cs
@@ -16,10 +16,12 @@ public class movement : MonoBehaviour
     private float distanceToGround;
     private int circleCount = 0, sphereCount = 0;
     private int circles = 0, spheres = 0;
-    public float secundomer;
+    public float timer;
+    public bool isGameFinished = false;
 
     void Start()
     {
+        //gameObject.transform.localEulerAngles = new Vector3(camObject.transform.localEulerAngles.x, camObject.transform.eulerAngles.y, camObject.transform.localEulerAngles.z);
         Debug.Log(SceneManager.GetActiveScene().name);
         if (SceneManager.GetActiveScene().name.Equals("Example_Terrain"))
         {
@@ -39,13 +41,16 @@ public class movement : MonoBehaviour
     {
         //gameObject.transform.rotation = new Quaternion((360 - camObject.transform.localEulerAngles.z)%360, (360 - camObject.transform.localEulerAngles.y) % 360, (360 - camObject.transform.localEulerAngles.x) % 360, camObject.transform.rotation.w);
         //gameObject.transform.rotation = new Quaternion(camObject.transform.rotation.z, camObject.transform.rotation.y, camObject.transform.rotation.x, camObject.transform.rotation.w);
+        //gameObject.transform.rotation = new Quaternion(camObject.transform.rotation.z, camObject.transform.rotation.y, camObject.transform.rotation.x, camObject.transform.rotation.w);
+        //gameObject.transform.rotation = new Quaternion(camObject.transform.rotation.z, camObject.transform.rotation.y, camObject.transform.rotation.x, camObject.transform.rotation.w);
         //gameObject.transform.position = new Vector3(transform.position.x + speed * (float)Math.Cos(camObject.transform.localEulerAngles.x * Math.PI/180),
         //transform.position.y + speed * (float)Math.Cos(camObject.transform.localEulerAngles.y * Math.PI / 180), transform.position.z + speed * (float)Math.Cos(camObject.transform.localEulerAngles.z * Math.PI / 180));
 
-        secundomer += Time.deltaTime;
+        
 
-        Debug.Log("Time: " + secundomer);
+        // Debug.Log("Time: " + timer);
 
+        ChangeTime();
         gameObject.transform.Rotate(360 - camObject.transform.localRotation.x * rotSpeed, camObject.transform.localRotation.y * rotSpeed, 360 - camObject.transform.localRotation.z * rotSpeed, Space.Self);
         gameObject.transform.Translate(Vector3.back * speed * Time.deltaTime);
 
@@ -70,27 +75,51 @@ public class movement : MonoBehaviour
     }
     public void OnTriggerEnter(Collider col)
     {
-        Debug.Log("Keeeeeeek " + col.gameObject.name);
+        //Debug.Log("Keeeeeeek " + col.gameObject.name);
         if (col.gameObject.name.Contains("Sphere"))
         {
             Destroy(col.gameObject);
             sphereCount++;
+            Debug.Log("Spheres collected: " + sphereCount);
         }
         if (col.gameObject.name.Contains("Circle"))
         {
             Destroy(col.gameObject);
             circleCount++;
+            Debug.Log("Circles collected: " + circleCount);
         }
 
-        if(circleCount == circles && sphereCount == spheres)
+        //if(circleCount == circles && sphereCount == spheres)
+        if (circleCount > 0)
         {
-            SceneManager.LoadScene("rpgpp_lt_scene");
+            isGameFinished = true;
+            StartCoroutine(ChangeLocationAfterTime(5));
         }
 
     }
     void OnGUI()
     {
-        GUI.contentColor = Color.black;
-        GUI.Label(new Rect(0,0,0,0), "Time: "+ secundomer);
+        GUIStyle guiStyle = new GUIStyle {fontSize = 40};
+        if (isGameFinished)
+        {
+            GUI.contentColor = Color.white;
+            GUI.backgroundColor = Color.black;
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+            GUI.Label(new Rect(Screen.width / 2f, Screen.height / 2f, 100, 100),
+                timer.ToString());
+        }
+    }
+
+    void ChangeTime()
+    {
+        if (isGameFinished) return;
+        timer += Time.deltaTime;
+    }
+    
+    IEnumerator ChangeLocationAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+ 
+        SceneManager.LoadScene("rpgpp_lt_scene");
     }
 }
